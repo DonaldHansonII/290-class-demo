716d658bf57c9640c319d7bcb6a0082d75429c19
donaldhansonii <donaldhanson@mail.adelphi.edu>
2016-03-22 21:49:37 +0000

    Fixing tag model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -108,6 +108,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :string, :body, :username)
+      params.require(:post).permit(:title, :string, :body, :username, :tag_titles)
     end
 end
